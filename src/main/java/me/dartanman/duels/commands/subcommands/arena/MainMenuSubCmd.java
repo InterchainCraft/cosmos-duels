48a6e0577d3647653babc892e468bcdc59cfa53f
Reece Williams <reecepbcups@gmail.com>
2023-08-12 14:12:49 -0500

    Show balance on main UI + stats

--- src/main/java/me/dartanman/duels/commands/subcommands/arena/MainMenuSubCmd.java
+++ src/main/java/me/dartanman/duels/commands/subcommands/arena/MainMenuSubCmd.java
@@ -15,6 +15,7 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import com.crafteconomy.blockchain.api.IntegrationAPI;
 import com.crafteconomy.blockchain.utils.Util;
 
 import me.dartanman.duels.Duels;
@@ -22,10 +23,11 @@ import me.dartanman.duels.commands.subcommands.DuelsSubCommand;
 import me.dartanman.duels.utils.UIHelper;
 
 public class MainMenuSubCmd extends DuelsSubCommand implements Listener
-{
-	private static Inventory menuInv;
+{	
     private String InvName = Util.color("&8&lDuels Menu");
-	private int rows = 3 * 9;
+	private int rows = 6 * 9;
+
+	private static IntegrationAPI api = IntegrationAPI.getInstance();
 
     private Map<Integer, String> slotToCommand;
 
@@ -35,19 +37,13 @@ public class MainMenuSubCmd extends DuelsSubCommand implements Listener
 
         Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
 
-		menuInv = Bukkit.createInventory(null, rows, InvName);
-
-        UIHelper.createDisplay(menuInv, Material.GRASS_BLOCK, 10, "&e&lSpawn", new ArrayList<String>());
-        UIHelper.createDisplay(menuInv, Material.DIAMOND_SWORD, 12, "&f&lArenas", new ArrayList<String>());        
-        UIHelper.createDisplay(menuInv, Material.IRON_CHESTPLATE, 14, "&7&oKits Coming Soon...", new ArrayList<String>());
-        UIHelper.createDisplay(menuInv, Material.BOOK, 16, "&e&lConnect Wallet", new ArrayList<String>());
-
         // create an array which links the slot number to the command to run for the player on click
         slotToCommand = new HashMap<Integer, String>();
         slotToCommand.put(10, "spawn");
         slotToCommand.put(12, "duels arenas");
         slotToCommand.put(14, "duels kits list");
-        slotToCommand.put(16, "gencode"); // from integration plugin
+        slotToCommand.put(16, "gencode"); // from integration plugin		
+        slotToCommand.put(28, "duels stats"); // from integration plugin		
     }
 
     @Override
@@ -59,10 +55,26 @@ public class MainMenuSubCmd extends DuelsSubCommand implements Listener
 		}
 
         Player p = (Player) sender;
- 		p.openInventory(menuInv);
+ 		p.openInventory(generateMainUI(p));
  		 				
         return true;
     }
+
+	private Inventory generateMainUI(Player p) {
+		Inventory menuInv = Bukkit.createInventory(null, rows, InvName);
+
+        UIHelper.createDisplay(menuInv, Material.GRASS_BLOCK, 10, "&e&lSpawn", new ArrayList<String>());
+        UIHelper.createDisplay(menuInv, Material.DIAMOND_SWORD, 12, "&f&lArenas", new ArrayList<String>());        
+        UIHelper.createDisplay(menuInv, Material.IRON_CHESTPLATE, 14, "&7&oKits Coming Soon...", new ArrayList<String>());
+        UIHelper.createDisplay(menuInv, Material.BOOK, 16, "&e&lConnect Wallet", new ArrayList<String>());
+        UIHelper.createDisplay(menuInv, Material.NAME_TAG, 28, "&e&lYour Duel Statistics", new ArrayList<String>());		
+
+		api.getCraftBalance(p.getUniqueId()).thenAccept(balance -> {
+			UIHelper.createDisplay(menuInv, Material.GOLD_INGOT, 34, "&e&l"+api.getTokenName()+" balance: " + balance, new ArrayList<String>());
+		});
+
+		return menuInv;
+	}
 
 	@EventHandler
 	public void onInventoryClick(InventoryClickEvent event) {
